b8508d85fc834ceb2fecb2335796165f5280a363
Claudio Restifo <claudio@samuiweb.net>
2019-04-17 16:54:25 +0700

    Updates

---
+++ precache-manifest.2fe9b8c69a0c7bb200ee8be4323e56a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5fe2164da04d47ef2c8",
+    "url": "/flappybird-react/static/css/main.bfa23fcc.chunk.css"
+  },
+  {
+    "revision": "f5fe2164da04d47ef2c8",
+    "url": "/flappybird-react/static/js/main.9090eacd.chunk.js"
+  },
+  {
+    "revision": "97bac731083db76262ff",
+    "url": "/flappybird-react/static/js/runtime~main.ef48449f.js"
+  },
+  {
+    "revision": "a252e227d3244d802167",
+    "url": "/flappybird-react/static/js/2.31a730b4.chunk.js"
+  },
+  {
+    "revision": "81c0aae9bb2eb54bf8fe07e1b68d829e",
+    "url": "/flappybird-react/static/media/PressStart2P-Regular.81c0aae9.ttf"
+  },
+  {
+    "revision": "46dad341d904387a4d38e2e1f05b20eb",
+    "url": "/flappybird-react/index.html"
+  }
+];
